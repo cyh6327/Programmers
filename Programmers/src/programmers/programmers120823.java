0555dc6b88266bbd14c85e2881aed5e431b342ae
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-04-03 10:36:39 +0900

    programmers120823 직각삼각형 출력하기

---
+++ Programmers/src/programmers/programmers120823.java
@@ -0,0 +1,20 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class programmers120823 {
+	
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
