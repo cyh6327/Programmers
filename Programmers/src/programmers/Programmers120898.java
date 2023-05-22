b979ba3b081b4672c6d68a14baf3326aeb065c5b
cyh6327 <cyh6327@gmail.com>
2023-05-22 09:30:50 +0900

    Programmers120898 편지

---
+++ Programmers/src/programmers/Programmers120898.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class Programmers120898 {
+	
+    public static void solution(String message) {
+        int answer = message.length()*2;
+        System.out.println(answer);
+    }
+    
+    public static void main(String[] args) {
+		solution("happy birthday!");
+		solution("I love you~");
+	}
+
+}
