a9260704d08f3e179c1b06eab51d3ac77beab6a1
cyh6327 <cyh6327@gmail.com>
2024-07-22 12:02:40 +0900

    Programmers12949 행렬의 곱셈

---
+++ Programmers/src/programmers/Programmers12949.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Programmers12949 {
+    public static void main(String[] args) {
+        int[][] arr1 = {{1,2}, {3,4}, {5,6}};
+        int[][] arr2 = {{1,2}, {3,4}};
+        System.out.println(Arrays.deepToString(solution(arr1, arr2)));
+
+        int[][] arr3 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr4 = {{3, 3}, {3, 3}};
+        System.out.println(Arrays.deepToString(solution(arr3, arr4)));
+    }
+
+    // O(n^3)
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int x = 0; x < arr1.length; x++) {
+            for (int y = 0; y < arr1[0].length; y++) {
+                int a1 = arr1[x][y];
+                for (int z = 0; z < arr2[0].length; z++) {
+                    int a2 = arr2[y][z];
+                    int temp = answer[x][z];
+                    answer[x][z] = temp + (a1 * a2);
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}
